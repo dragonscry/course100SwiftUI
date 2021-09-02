e64e34a8603ee8b9066a08c1b00dd212f348e4a0
dragonscry <denismerkotun@gmail.com>
2021-09-02 09:49:31 +0300

    5 project

---
+++ WorldScramble/WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Denys on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
