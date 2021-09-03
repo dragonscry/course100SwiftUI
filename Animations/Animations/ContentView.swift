20a07b2dda54c4d4f49a61c7ec1ad9964f95be35
dragonscry <denismerkotun@gmail.com>
2021-09-03 21:08:01 +0300

    challenge for animation on Guess the Flag

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier{
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition{
+    static var pivot: AnyTransition{
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
+    @State private var seconds = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            withAnimation{
+                seconds += 360
+            }
+        }, label: {
+            Rectangle().frame(width: 10, height: 100)
+        })
+        .rotationEffect(.degrees(Double(seconds)), anchor: .bottom)
+
+        
     }
 }
 
--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -31,6 +31,10 @@ struct ContentView: View {
     @State private var score = 0
     @State private var message = ""
     
+    @State private var animationAmount = 0.0
+    @State private var wrongAmount = 0.0
+    @State private var opacity = 1.0
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).ignoresSafeArea(.all)
@@ -50,6 +54,18 @@ struct ContentView: View {
                     }) {
                         FlagImage(image: self.countries[number])
                     }
+                    .rotation3DEffect( number == correctAnswer ?
+                        Angle.degrees(animationAmount): Angle.degrees(0),
+                        axis: (x: 0.0, y: 1.0, z: 0.0)
+                    )
+                    .opacity(number != correctAnswer ? opacity : 1)
+                    .rotation3DEffect(
+                        .degrees(wrongAmount),
+                        axis: (x: 0.0, y: 0.0, z: 1.0),
+                        anchor: .bottom,
+                        anchorZ: 0,
+                        perspective: 1
+                    )
                     
                 }
                 
@@ -77,16 +93,31 @@ Your score is \(score)
             scoreTitle = "Correct!"
             score += 1
             message = "You are right!"
+            withAnimation(.interpolatingSpring(stiffness: 30, damping: 10)) {
+                animationAmount = 360
+                
+            }
+            withAnimation{
+                opacity = 0.25
+            }
+            
         }else {
             scoreTitle = "Wrong!"
             score += -1
             message = "That's the flag of \(countries[number])!"
+            withAnimation(.interpolatingSpring(stiffness: 30, damping: 10)){
+                wrongAmount += 360
+            }
         }
         showingScore = true
     }
     
     func askQuestion() {
         countries.shuffle()
+        animationAmount = 0
+        withAnimation{
+            opacity = 1
+        }
         correctAnswer  = Int.random(in: 0...2)
     }
 }
