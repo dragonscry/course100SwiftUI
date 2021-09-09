18c3346087c3c09d6b6b676496ad6f0d49383514
dragonscry <denismerkotun@gmail.com>
2021-09-09 22:57:03 +0300

    first part 8 Proj

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Denys on 09.09.2021.
+//
+
+import Foundation
+
+struct Astronaut : Codable, Identifiable {
+    let id: String
+    let name : String
+    let description : String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Denys on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
