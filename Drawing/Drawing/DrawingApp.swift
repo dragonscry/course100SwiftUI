1d271cdb312e64f89bad51d7b50c7f17e449dc7e
dragonscry <denismerkotun@gmail.com>
2021-09-14 22:35:59 +0300

    spirograph

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Denys on 12.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
