f72a42dc9a6bc31fe9b2f786fd6d0a61896f5d07
dragonscry <denismerkotun@gmail.com>
2021-09-16 22:03:45 +0300

    add arrow

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -5,92 +5,41 @@
 //  Created by Denys on 12.09.2021.
 //
 
-import SwiftUI
 
-
-struct Spirograph: Shape {
-    let innerRadius: Int
-    let outerRadius: Int
-    let distance: Int
-    let amount: CGFloat
+struct Arrow : Shape {
     
-    func gcd(_ a : Int, _ b : Int)->Int{
-        var a = a
-        var b = b
-        
-        while b != 0 {
-            let temp = b
-            b = a % b
-            a = temp
-        }
-        
-        return a
-    }
+    var amount : CGFloat = 3
+    var with : CGFloat = 1.4
     
     func path(in rect: CGRect) -> Path {
-        let divisor = gcd(innerRadius, outerRadius)
-        let outerRadius = CGFloat(self.outerRadius)
-        let innerRadius = CGFloat(self.innerRadius)
-        let distance = CGFloat(self.distance)
-        let difference = innerRadius - outerRadius
-        let endPoint = ceil(2 * CGFloat.pi * outerRadius/CGFloat(divisor)) * amount
         var path = Path()
         
-        for theta in stride(from: 0, through: endPoint, by: 0.01){
-            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
-            
-            x += rect.width / 2
-            y += rect.height / 2
-            
-            if theta == 0 {
-                path.move(to: CGPoint(x:x, y:y))
-            }else {
-                path.addLine(to: CGPoint(x:x, y:y))
-            }
-        }
+        var triangle = Path()
+        triangle.move(to: CGPoint(x: rect.maxX/with , y: rect.minY + rect.maxY/7))
+        triangle.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        triangle.addLine(to: CGPoint(x: rect.maxX/with, y: rect.maxY - rect.maxY/7))
+        triangle.addLine(to: CGPoint(x: rect.maxX/with, y: rect.minY + rect.maxY/7))
+
+      var rect = CGRect(x: rect.minX, y: rect.minY+(rect.maxX/amount), width: rect.maxY/with, height: rect.maxX-(rect.maxX/amount)*2)
+        
+        path.addRect(rect)
+        path.addPath(triangle)
         
         return path
     }
 }
 
+
+import SwiftUI
+
 struct ContentView: View {
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount : CGFloat = 1.0
-    @State private var hue = 0.6
     
     var body: some View {
         VStack(spacing: 0){
-            Spacer()
-            
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+            Arrow()
                 .frame(width: 300, height: 300)
-            Spacer()
+                
             
-            Group{
-                Text("Inner Radius: \(Int(innerRadius))")
-                Slider(value: $innerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Outer Radius: \(Int(outerRadius))")
-                Slider(value: $outerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Distance: \(Int(distance))")
-                Slider(value: $distance, in: 1...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Amount: \(amount,specifier: "%.2f")")
-                Slider(value: $amount)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Color")
-                Slider(value: $hue)
-                    .padding(.horizontal)
-            }
         }
     }
 }
