bd2f1f4ed434958b26604d19196c7fd6a38b701c
dragonscry <denismerkotun@gmail.com>
2021-09-19 23:10:01 +0300

    first of 10 project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Denys on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
