95e30a634226f7cc930c93f279c525b43d53dfda
dragonscry <denismerkotun@gmail.com>
2021-09-20 21:49:27 +0300

    begin cupcake

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,21 @@
+//
+//  AddresView.swift
+//  CupcakeCorner
+//
+//  Created by Denys on 20.09.2021.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: Order())
+    }
+}
--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,29 +7,51 @@
 
 import SwiftUI
 
-class User : ObservableObject, Codable {
-    enum CodingKeys : CodingKey {
-        case name
-    }
-    
-    @Published var name = "Paul Hudson"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey : .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
 
 struct ContentView: View {
+    
+    @ObservedObject var order = Order()
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type){
+                        ForEach(0..<Order.types.count, id: \.self){
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()){
+                        Text("Any Special Requestes?")
+                    }
+                    
+                    if order.specialRequestEnabled{
+                        Toggle(isOn: $order.extraFrosting){
+                            Text("Add Extra Frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles){
+                            Text("Add Extra Sprinkles?")
+                        }
+                    }
+                    Section{
+                        NavigationLink(destination: AddressView(order: order)){
+                            Text("Delivery Details")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
