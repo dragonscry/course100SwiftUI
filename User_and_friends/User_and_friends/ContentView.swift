01c44093d9570ff01396cae4996d7ff16aefb2ff
dragonscry <denismerkotun@gmail.com>
2021-10-03 23:05:16 +0300

    updated to working version

--- User_and_friends/User_and_friends/ContentView.swift
+++ User_and_friends/User_and_friends/ContentView.swift
@@ -30,12 +30,16 @@ struct ContentView: View {
         let request = URLRequest(url: url)
         URLSession.shared.dataTask(with: request){data, response, error in
             if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
+                if let decodedResponse = try? JSONDecoder().decode([User].self, from: data){
                     DispatchQueue.main.async {
-                        self.users = decodedResponse.results
+                        self.users = decodedResponse
                     }
+                    
+                    return
                 }
             }
+            print ("Fetch failed: \(error?.localizedDescription ?? "Unknown Error")")
+            
         }.resume()
     }
 }
--- User_and_friends/User_and_friends/User.swift
+++ User_and_friends/User_and_friends/User.swift
@@ -8,20 +8,20 @@
 import Foundation
 
 struct User : Codable {
-    let id : String
-    let isActive: Bool
-    let name: String
-    let age: Int
-    let company : String
-    let email : String
-    let address: String
-    let about: String
-    let registered: String
-    let tags: [String]
-    let friends: [Friend]
+    var id : String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company : String
+    var email : String
+    var address: String
+    var about: String
+    var registered: String
+    var tags: [String]
+    //var friends: [Friend]
 }
 
 struct Friend : Codable {
-    let id: String
-    let name: String
+    var id: String
+    var name: String
 }
