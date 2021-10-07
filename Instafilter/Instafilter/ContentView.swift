b21a42d1a65343ec3cd22a757a44770420834bd6
dragonscry <denismerkotun@gmail.com>
2021-10-07 21:59:44 +0300

    basic UI

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -21,29 +21,50 @@ class ImageSaver: NSObject {
 struct ContentView: View {
     
     @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
-        VStack{
-            image?
-                .resizable().scaledToFit()
-            Button("Select Image"){
-                self.showingImagePicker = true
+        NavigationView{
+            VStack{
+                ZStack{
+                    Rectangle()
+                        .fill(Color.secondary)
+                    
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    
+                }
+                
+                HStack{
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack{
+                    Button("Change Filter") {
+                        //
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save"){
+                        //
+                    }
+                }
             }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage){
-            ImagePicker(image: self.$inputImage)
-        }
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else {
-            return
-        }
-        image = Image(uiImage: inputImage)
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
