c5975c2aeba7767bb6aed5157819191deed12776
dragonscry <denismerkotun@gmail.com>
2021-10-11 21:54:36 +0300

    end of 66 day end a project

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,26 +8,17 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-class ImageSaver: NSObject {
-    func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-        
-    }
-    
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save Finished")
-    }
-}
-
 struct ContentView: View {
     
     @State private var image: Image?
     @State private var filterIntensity = 0.5
     
+    @State private var showingFilterSheet = false
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
     
-    @State var currentFilter = CIFilter.sepiaTone()
+    @State var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
     
     var body: some View {
@@ -68,13 +59,20 @@ struct ContentView: View {
                 
                 HStack{
                     Button("Change Filter") {
-                        //
+                        self.showingFilterSheet = true
                     }
                     
                     Spacer()
                     
                     Button("Save"){
-                        //
+                        guard let processedImage = self.processedImage else {
+                            return
+                        }
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = {print("Success")}
+                        imageSaver.errorHandler = {print("Oops: \($0.localizedDescription)")}
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
+
                     }
                 }
             }
@@ -82,6 +80,18 @@ struct ContentView: View {
             .navigationBarTitle("Instafilter")
             .sheet(isPresented: $showingImagePicker, onDismiss: loadImage){
                 ImagePicker(image: self.$inputImage)
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallize")) {self.setFilter(CIFilter.crystallize())},
+                    .default(Text("Edges")) {self.setFilter(CIFilter.edges())},
+                    .default(Text("Gaussian Blur")) {self.setFilter(CIFilter.gaussianBlur())},
+                    .default(Text("Pixellate")) {self.setFilter(CIFilter.pixellate())},
+                    .default(Text("Sepia Tone")) {self.setFilter(CIFilter.sepiaTone())},
+                    .default(Text("Unsharp Mask")) {self.setFilter(CIFilter.unsharpMask())},
+                    .default(Text("Vignette")) {self.setFilter(CIFilter.vignette())},
+                    .cancel()
+                ])
             }
         }
     }
@@ -95,15 +105,22 @@ struct ContentView: View {
     }
     
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
-        
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) {currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)}
+        if inputKeys.contains(kCIInputRadiusKey) {currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey) {currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)}
         guard let outputImage = currentFilter.outputImage
         else {return}
         
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
+    }
+    func setFilter(_ filter: CIFilter){
+        currentFilter = filter
+        loadImage()
     }
 }
 
