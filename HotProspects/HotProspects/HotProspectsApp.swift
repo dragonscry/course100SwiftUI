25fb67518a9fdcdfecdec9e4501b74e8c888a82a
dragonscry <denismerkotun@gmail.com>
2021-11-02 19:57:03 +0200

    completed day 80

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Denys on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
