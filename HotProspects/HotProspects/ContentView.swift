d089a82b9e54d20d21e940c5558790615df3d1af
dragonscry <denismerkotun@gmail.com>
2021-11-07 13:50:24 +0200

    context menu

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello Swift")
+                .padding()
+                .background(backgroundColor)
+            Text("Change Color")
+                .padding()
+                .contextMenu{
+                    Button(action: {
+                        self.backgroundColor = .red
+                    }) {
+                        Text("Red")
+                    }
+                    Button(action: {
+                        self.backgroundColor = .green
+                    }) {
+                        Text("Green")
+                    }
+                    Button(action: {
+                        self.backgroundColor = .yellow
+                    }) {
+                        Text("Yellow")
+                    }
+                }
+        }
     }
 }
 
