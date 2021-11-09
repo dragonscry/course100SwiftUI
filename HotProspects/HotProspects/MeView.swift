20bd647bddef2c827f05e6baf628ab5f13d5a5f7
dragonscry <denismerkotun@gmail.com>
2021-11-09 21:58:47 +0200

    add tabview

---
+++ HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Denys on 09.11.2021.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("Me")
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
