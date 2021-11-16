b0f5af65d3f11ce81ff525e17138257147623146
dragonscry <denismerkotun@gmail.com>
2021-11-16 15:19:12 +0200

    new proj

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Denys on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
