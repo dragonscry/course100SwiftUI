1b6249db01c5bc0d145d03c6eee555a45171082d
dragonscry <denismerkotun@gmail.com>
2021-11-24 22:37:12 +0200

    add generator

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -16,6 +16,8 @@ struct CardView: View {
     
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    @State private var feedback = UINotificationFeedbackGenerator()
+    
     
     var body: some View {
         ZStack{
@@ -51,9 +53,15 @@ struct CardView: View {
                     .onChanged{
             gesture in
             self.offset = gesture.translation
+            self.feedback.prepare()
         }
                     .onEnded{ _ in
             if abs(self.offset.width) > 100 {
+                if self.offset.width > 0 {
+                    self.feedback.notificationOccurred(.success)
+                } else {
+                    self.feedback.notificationOccurred(.error)
+                }
                 self.removal?()
             } else {
                 self.offset = .zero
