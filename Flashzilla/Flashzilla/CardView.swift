38daad01fffbfb962668a3819cd21f246381c49b
dragonscry <denismerkotun@gmail.com>
2021-11-26 23:18:07 +0200

    fix bugs

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CardView: View {
     
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
     let card: Card
     
     var removal : (() -> Void)? = nil
@@ -24,21 +25,26 @@ struct CardView: View {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
                 .fill(
                     differentiateWithoutColor ? Color.white :
-                Color.white.opacity(1-Double(abs(offset.width/50))))
+                        Color.white.opacity(1-Double(abs(offset.width/50))))
                 .background(
                     differentiateWithoutColor ? nil : RoundedRectangle(cornerRadius: 25, style: .continuous)
                         .fill(offset.width > 0 ? Color.green: Color.red)
                 )
                 .shadow(radius: 10)
             VStack{
-                Text(card.prompt)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-                
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.gray)
+                if accessibilityEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle).foregroundColor(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                    
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
                 }
                 
             }
@@ -49,6 +55,7 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibility(addTraits: .isButton)
         .gesture(DragGesture()
                     .onChanged{
             gesture in
@@ -71,6 +78,7 @@ struct CardView: View {
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
+        .animation(.spring())
     }
 }
 
