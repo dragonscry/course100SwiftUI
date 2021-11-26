0b9666e8526740dbf96d981a7bc72c6ddfefd203
dragonscry <denismerkotun@gmail.com>
2021-11-27 01:15:10 +0200

    end proj

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -17,11 +17,13 @@ extension View {
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @Environment(\.accessibilityEnabled) var accessibilityEnabled
-    @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var cards = [Card]()
     
     @State private var isActive = true
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var showingEditScreen = false
     
     var body: some View {
         ZStack{
@@ -64,6 +66,25 @@ struct ContentView: View {
                 }
                 
             }
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.showingEditScreen = true
+                        
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .clipShape(Circle())
+                    }
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
             
             if differentiateWithoutColor || accessibilityEnabled {
                 VStack {
@@ -120,6 +141,11 @@ struct ContentView: View {
                 self.isActive = true
             }
         }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards){
+            EditCard()
+        }
+        .onAppear(perform: resetCards)
+        
     }
     
     func removeCard(at index: Int)
@@ -136,6 +162,15 @@ struct ContentView: View {
         cards = [Card](repeating: Card.example, count: 10)
         timeRemaining = 100
         isActive = true
+        loadData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                self.cards = decoded
+            }
+        }
     }
 }
 
