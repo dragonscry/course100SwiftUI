2cec99098bd167c658505db732999d764b4a7dea
dragonscry <denismerkotun@gmail.com>
2021-12-14 21:21:37 +0200

    start project

---
+++ ShowSeeker/ShowSeeker/ResortDetailsView.swift
@@ -0,0 +1,40 @@
+//
+//  ResortDetailsView.swift
+//  ShowSeeker
+//
+//  Created by Denys on 14.12.2021.
+//
+
+import SwiftUI
+
+struct ResortDetailsView: View {
+    let resort: Resort
+    
+    var size: String {
+        switch resort.size {
+        case 1:
+            return "Small"
+        case 2:
+            return "Average"
+        default:
+            return "Large"
+        }
+    }
+    
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Size: \(size)")
+            Text("Price: \(price)")
+        }
+    }
+}
+
+struct ResortDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortDetailsView(resort: Resort.example)
+    }
+}
---
+++ ShowSeeker/ShowSeeker/SkiDetailsView.swift
@@ -0,0 +1,26 @@
+//
+//  SkiDetailsView.swift
+//  ShowSeeker
+//
+//  Created by Denys on 14.12.2021.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    
+    let resort: Resort
+    
+    var body: some View {
+        VStack {
+            Text("Elevation: \(resort.elevation)m")
+            Text("Snow: \(resort.snowDepth)cm")
+        }
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkiDetailsView(resort: Resort.example)
+    }
+}
